b8f2e2a62e0ce899d99ecca7be2e218b5b9c0d5d
abidikhairi <abidikhairi.1337@gmail.com>
2023-01-20 21:08:59 +0100

    visualize the positional encoding layer

---
+++ projects/nmt/run_function.py
@@ -0,0 +1,26 @@
+import sys
+import torch as th
+import matplotlib.pyplot as plt
+from src.parts import PositionalEncoding
+
+
+def positional_encoding():
+    max_position = 256
+    d_model=16
+    pe = PositionalEncoding(d_model=d_model, dropout=0.0)
+    x = th.zeros(max_position, d_model)
+
+    out = pe(x)
+    out = out[1].numpy()
+
+    plt.plot()
+    plt.imshow(out, aspect='auto', interpolation='nearest')
+    plt.xlabel("Position")
+    plt.ylabel("Encoding")
+    plt.colorbar()
+    plt.savefig("figures/positional_encoding.png")
+
+
+if __name__ == "__main__":
+    function_name = sys.argv[1]
+    getattr(sys.modules[__name__], function_name)()
