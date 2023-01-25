b986db1ce88fe77320efbad559759a46372abb83
abidikhairi <abidikhairi.1337@gmail.com>
2023-01-25 22:03:05 +0100

    implement training_step & validation_step

---
+++ projects/nmt/src/training.py
@@ -0,0 +1,36 @@
+import torch
+from torchmetrics import BLEUScore
+
+
+def training_step(model, compute_loss, optimizer, scheduler, batch, device):
+    optimizer.zero_grad()
+
+    src, src_mask, tgt, tgt_mask, tgt_y, seq_len = batch(device=device)
+
+    output = model(src, src_mask, tgt, tgt_mask)
+    loss = compute_loss(output, tgt, norm=seq_len)
+
+    loss.backward()
+    optimizer.step()
+    scheduler.step()
+
+    return loss
+
+
+def validation_step(model, compute_loss, batch, device, tgt_tokenizer):
+    src, src_mask, tgt, tgt_mask, tgt_y, seq_len = batch(device=device)
+
+    output = model(src, src_mask, tgt, tgt_mask)
+    loss = compute_loss(output, tgt, norm=seq_len)
+
+    output = model.validation_step(src, src_mask, tgt, tgt_mask)
+
+    preds = torch.argmax(output, dim=-1)
+
+    preds = tgt_tokenizer.batch_decode(preds)
+    references = tgt.batch_decode(tgt)
+
+    bleu_score = BLEUScore(n_gram=2)
+    score = bleu_score(preds, references)
+
+    return loss, score
