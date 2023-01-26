de217c70651d15994fdb3cbe0431a906755ac527
abidikhairi <abidikhairi.1337@gmail.com>
2023-01-26 21:24:42 +0100

    add pydantic models to handle config

---
+++ projects/nmt/src/config.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+
+
+class Seq2SeqConfig(BaseModel):
+    num_encoder_layers: int = 2
+    num_decoder_layers: int = 2
+    d_model: int = 1024
+    nhead: int = 8
+    dim_feedforward: int = 512
+    dropout: int = 0.1
+    pad_token: int = 0
+
+
+class OptimizerConfig(BaseModel):
+    lr: float
+    weight_decay: float
+    beta1: float
+    beta2: float
+    eps: float
+    warmup_steps: int = 4000
+    d_model: int = 1024
+
+
+class TrainingConfig(BaseModel):
+    device: str
+    src: str
+    tgt: str
+    dataset: str
+    dataset_name: str
+    src_tokenizer: str
+    tgt_tokenizer: str
+    train_batch_size: int
+    valid_batch_size: int
+    log_every_n_steps: int
+    smoothing_factor: float
